cfe12a61a949501cd254b0a944fa5469878ce5d4
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-21 17:50:47 +0200

    Added c starter kit

---
+++ MedievalFootball.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    // The corner of the map representing your base
+    int base_x;
+    int base_y;
+    scanf("%d%d", &base_x, &base_y);
+    // Always 3
+    int heroes_per_player;
+    scanf("%d", &heroes_per_player);
+
+    // game loop
+    while (1) {
+        for (int i = 0; i < 2; i++) {
+            // Your base health
+            int health;
+            // Ignore in the first league; Spend ten mana to cast a spell
+            int mana;
+            scanf("%d%d", &health, &mana);
+        }
+        // Amount of heros and monsters you can see
+        int entity_count;
+        scanf("%d", &entity_count);
+        for (int i = 0; i < entity_count; i++) {
+            // Unique identifier
+            int id;
+            // 0=monster, 1=your hero, 2=opponent hero
+            int type;
+            // Position of this entity
+            int x;
+            int y;
+            // Ignore for this league; Count down until shield spell fades
+            int shield_life;
+            // Ignore for this league; Equals 1 when this entity is under a control spell
+            int is_controlled;
+            // Remaining health of this monster
+            int health;
+            // Trajectory of this monster
+            int vx;
+            int vy;
+            // 0=monster with no target yet, 1=monster targeting a base
+            int near_base;
+            // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+            int threat_for;
+            scanf("%d%d%d%d%d%d%d%d%d%d%d", &id, &type, &x, &y, &shield_life, &is_controlled, &health, &vx, &vy, &near_base, &threat_for);
+        }
+        for (int i = 0; i < heroes_per_player; i++) {
+
+            // Write an action using printf(). DON'T FORGET THE TRAILING \n
+            // To debug: fprintf(stderr, "Debug messages...\n");
+
+
+            // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
+            printf("MOVE 420 420\n");
+        }
+    }
+
+    return 0;
+}
