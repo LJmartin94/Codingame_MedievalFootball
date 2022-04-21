dbb1d030b8d87a5cf77bf0612c84e71ca24ee26d
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-04-21 22:22:26 +0200

    Added a distance to base attribute to the entity struct. Made my defenders target the nearest creep that isn't headed towards my enemy, except for one defender who always stays in base and targets monsters only once they are near. This is my first Arena submit.

--- MedievalFootball.c
+++ MedievalFootball.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <limits.h>
 
 typedef struct s_entity
 {
@@ -12,6 +13,8 @@ typedef struct s_entity
     int health; // Remaining health of this monster
     int near_base; // 0=monster with no target yet, 1=monster targeting a base
     int threat_for; // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+
+    int dist_to_base;
     
     // Position of this entity
     int x;
@@ -41,7 +44,8 @@ int main()
 
     // game loop
     while (1) {
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 2; i++) 
+        {
             // Your base health
             int health;
             // Ignore in the first league; Spend ten mana to cast a spell
@@ -84,15 +88,32 @@ int main()
             peepz[i].y = y;
             peepz[i].vx = vx;
             peepz[i].vy = vy;
+            peepz[i].dist_to_base = dist(x, y, base_x, base_y);
         }
-        for (int i = 0; i < heroes_per_player; i++) {
+
+        t_entity target = peepz[0];
+        int nearest = INT_MAX;
+        for (int i = 0; i < entity_count; i++) 
+        {
+            if(peepz[i].type == 0 && peepz[i].threat_for != 2 && peepz[i].dist_to_base < nearest)
+            {
+                target = peepz[i];
+                nearest = peepz[i].dist_to_base;
+            }
+        }
+
+        for (int i = 0; i < heroes_per_player; i++) 
+        {
 
             // Write an action using printf(). DON'T FORGET THE TRAILING \n
             // To debug: fprintf(stderr, "Debug messages...\n");
 
 
             // In the first league: MOVE <x> <y> | WAIT; In later leagues: | SPELL <spellParams>;
-            printf("MOVE 420 420\n");
+            if (target.type == 0 && (i != 0 || target.near_base))
+                printf("MOVE %d %d\n", target.x, target.y);
+            else
+                printf("MOVE %d %d\n", base_x, base_y);
         }
     }
 
